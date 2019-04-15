73ef484e62cd9d0b1ce1d8edcdbe55351f074764
cww <wxc4295@g.rit.edu>
2019-04-15 00:17:02 -0400

    Add the demo1

---
+++ demo111/Assets/Scripts/ChangePlaneAnchorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangePlaneAnchorScript : MonoBehaviour {
+    public ContentPositioningBehaviour behavior;
+    public AnchorBehaviour anchor;
+
+    public void ChangePlaneAnchor()
+    {
+        behavior.AnchorStage = anchor;
+    }
+}
