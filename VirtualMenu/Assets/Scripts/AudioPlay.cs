29538cd384fe57fb66b568e59ba4fe28b44f1bcd
Wenwen Cheng (RIT Student) <wxc4295@ad.rit.edu>
2019-05-01 20:07:34 -0400

    Wenwen:add id for models Lydia:add sound

---
+++ VirtualMenu/Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    // declare audio 
+
+    public AudioSource buttonClick;
+
+    public void soundPlay()
+    {
+        buttonClick.Play();
+
+    }
+}
